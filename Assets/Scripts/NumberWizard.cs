d763143c226254a1ac2615bf617543537645992f
Derek Hirotsu <55931997+derekhirotsu@users.noreply.github.com>
2023-04-03 16:57:20 -0700

    Add project

---
+++ Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NumberWizard : MonoBehaviour {
+  [SerializeField] int max;
+  [SerializeField] int min;
+  [SerializeField] TextMeshProUGUI guessText;
+  int guess;
+
+  void Start () {
+    StartGame();
+	}
+
+  void StartGame () {
+    max = max + 1; // Accomodates Random.Range upper limit being exclusive.
+    NextGuess();
+  }
+
+  void NextGuess () {
+    if (min < max) {
+      guess = Random.Range(min, max);
+      guessText.text = guess.ToString();
+    } else {
+      min = guess;
+    }
+  }
+
+  public void OnPressHigher() {
+    min = guess + 1;
+    NextGuess();
+  }
+
+  public void OnPressLower () {
+    max = guess;
+    NextGuess();
+  }
+}
